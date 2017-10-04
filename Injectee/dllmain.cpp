1b0ce9c4929d6f08a668b87e64f11e2c6569a595
Umang Raghuvanshi <u@umangis.me>
2017-10-04 20:47:27 +0530

    Add the main application

--- Injectee/dllmain.cpp
+++ Injectee/dllmain.cpp
@@ -2,29 +2,60 @@
 #include "stdafx.h"
 #include <easyhook.h>
 #include <iostream>
+#include <assert.h>
+#include <fstream>
+#include <comdef.h>
 
 int64_t(*gOriginalPrintChat)(void *, int, WCHAR*, int);
 
+std::fstream* gLogFile;
+HOOK_TRACE_INFO ghHook;
+HANDLE ghPipe;
+
+void Log(char* msg) {
+#ifdef DEBUG
+	*gLogFile << msg << "\r\n";
+	gLogFile->flush();
+#endif
+}
+
+void WriteChatMsgToNamedPipe(char* msg) {
+	WriteFile(ghPipe, msg, strlen(msg), NULL, NULL);
+}
+
 int64_t __fastcall printChatHook(void *a1, int a2, WCHAR* message, int a4) {
-	std::cout << message;
+	_bstr_t narrow(message);
+	WriteChatMsgToNamedPipe(narrow);
 	return gOriginalPrintChat(a1, a2, message, a4);
 }
 
 extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo) {
-	HMODULE hClient = GetModuleHandle(TEXT("client.dll"));
-	gOriginalPrintChat = (int64_t(*) (void *, int, WCHAR*, int))(hClient + 0x180A588E0);
-	HOOK_TRACE_INFO hHook = { NULL };
+#ifdef DEBUG
+	gLogFile = new std::fstream("dt.log", std::ios::out);
+#endif
 
-	std::cout << "Found printChat at: " << std::hex << gOriginalPrintChat << std::endl;
-	std::cout << "Hooking...." << std::endl;
-
-	NTSTATUS didHook = LhInstallHook(gOriginalPrintChat, printChatHook, NULL, &hHook);
-	if (FAILED(didHook))
-		std::cout << "Failed to hook printChat :(" << std::endl;
+	ghPipe = CreateFile(TEXT("\\\\.\\pipe\\DotATranslator"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	if (ghPipe == INVALID_HANDLE_VALUE)
+		Log("Can't connect to the pipe");
 	else
-		std::cout << "Hooked printChat successfully" << std::endl;
+		Log("Connected to named pipe");
 
-	LhSetExclusiveACL({}, 0, &hHook);
+	HMODULE hClient = GetModuleHandle(TEXT("client.dll"));
+	FARPROC ba = GetProcAddress(hClient, "BinaryProperties_GetValue");
+
+	gOriginalPrintChat = (int64_t(*) (void *, int, WCHAR*, int))((int64_t)ba + 0x358cc0);
+	ghHook = { NULL };
+
+	Log("Hooking....");
+
+	NTSTATUS didHook = LhInstallHook(gOriginalPrintChat, printChatHook, NULL, &ghHook);
+	if (FAILED(didHook))
+		Log("Failed to hook printChat :(");
+	else
+		Log("Hooked printChat successfully");
+
+	ULONG ACLEntries[1] = { 0 };
+	LhSetExclusiveACL(ACLEntries, 1, &ghHook);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
---
+++ Translator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+using EasyHook;
+using System.IO;
+
+namespace Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DotA 2 Translator");
+
+            int targetPID = 0;
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                try
+                {
+                    if (processes[i].MainWindowTitle == "Dota 2" && processes[i].HasExited == false)
+                    {
+                        targetPID = processes[i].Id;
+                    }
+                }
+                catch { }
+            }
+            if (targetPID == 0)
+            {
+                Console.WriteLine("You see, you need to run the game for the translator to do something");
+                Console.ReadKey();
+                Environment.Exit(-1);
+            }
+
+            try
+            {
+                NativeAPI.RhInjectLibrary(targetPID, 0, NativeAPI.EASYHOOK_INJECT_DEFAULT, "", ".\\Injectee.dll", IntPtr.Zero, 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine("GLHF!");
+
+            while (true)
+            {
+                var server = new NamedPipeServerStream("DotATranslator");
+                server.WaitForConnection();
+                StreamReader reader = new StreamReader(server);
+                while (true)
+                {
+                    var chatMsg = reader.ReadLine();
+                    Console.WriteLine(chatMsg);
+                }
+            }
+        }
+    }
+}
